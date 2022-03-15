2efdf5bf3b2508baf96d3a100c999dc07ed25495
Marcelo-RM <marcelordrgs98@gmail.com>
2022-03-15 12:17:07 -0300

    Adicionar arquivos de projeto.

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using curso.api.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginInput login)
+        {
+            return Created("", login);
+        }
+    }
+}
---
+++ curso.api/Model/LoginInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Model
+{
+    public class LoginInput
+    {
+        public string Login { get; set; }
+        public string Pass { get; set; }
+    }
+}
